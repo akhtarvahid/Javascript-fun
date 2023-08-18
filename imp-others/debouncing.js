5cc737ce5c634e3eee3798c81eff5d640673cf24
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-08-19 00:31:47 +0530

    debouncing

---
+++ imp-others/debouncing.js
@@ -0,0 +1,11 @@
+function debounce(func, wait) {
+    let timerId;
+    return (...args) => {
+        if (timerId) {
+              clearTimeout(timerId);
+          }
+          timerId = setTimeout(() => {
+              func(...args);
+        }, wait);
+    };
+}
