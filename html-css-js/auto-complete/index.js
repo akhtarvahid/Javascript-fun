309a67a18ccbc49c979d3bfb46c4b4b23d006378
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-08-25 00:46:41 +0530

    search functionality

---
+++ html-css-js/auto-complete/index.js
@@ -0,0 +1,35 @@
+import getCountries from "./fetchData.js";
+
+const inputBox = document.getElementById("input");
+const suggestionBox = document.getElementById("suggestions");
+
+const searchHandler = async (keyword) => {
+    const countries = await getCountries(keyword);
+    const countryNames = countries.map((country) => country.name.common);
+    return countryNames;
+}
+
+const populateSuggestionBox = (countryNames) => {
+    if (countryNames.length) {
+        suggestionBox.classList.add("visible");
+    } else {
+        suggestionBox.classList.remove("visible");
+    }
+    suggestionBox.innerHTML = "";
+
+    const fragment = document.createDocumentFragment();
+    console.log('countryNames array', countryNames)
+    countryNames.forEach((countryName) => {
+        const li = document.createElement("li");
+        li.innerText = countryName;
+        fragment.appendChild(li);
+    })
+    suggestionBox.appendChild(fragment);
+}
+
+const suggestionHandler = async (e) => {
+    const keyword = e.target.value;
+    const countryNames = await searchHandler(keyword);
+    populateSuggestionBox(countryNames);
+}
+inputBox.addEventListener("input", suggestionHandler);
