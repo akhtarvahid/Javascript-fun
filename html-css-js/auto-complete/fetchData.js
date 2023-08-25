85eeb77ac1c049858c66ad1a4d13f6e172c06a14
Vahid Akhtar <akhtarvahid543@gmail.com>
2023-08-25 18:13:16 +0530

    autocomplete refactor

--- html-css-js/auto-complete/fetchData.js
+++ html-css-js/auto-complete/fetchData.js
@@ -3,10 +3,7 @@ async function getCountries(keyword) {
         const response = await fetch(`https://restcountries.com/v3.1/name/${keyword}`);
         const result = await response.json();
         if (response.status == 404) {
-            console.log("Page not found");
             return [];
-        } else {
-            console.log("Data found");;
         }
         return result;
     } catch (err) {
